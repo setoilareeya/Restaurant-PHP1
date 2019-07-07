502913262a94f1761f6050da0c3415605606e0ff
setoilareeya <setoilareeya@gmail.com>
2019-07-08 06:46:29 +0700

    Restaurant-PHP
    Restaurant-PHP

---
+++ Restaurant-PHP/includes/footer.php
@@ -0,0 +1,45 @@
+			<div id="footer" class="cf">
+
+				<div class="column three">
+					<strong>Phone</strong>
+					094-398-9545
+				</div><!-- column -->
+
+				<div class="column three">
+					<strong>Location</strong>
+					92, Floor 2<br>
+					Sathorn Thani 2 Building, North Sathorn Road, Silom, Bangrak, Bangkok, Thailand 10500
+				</div><!-- column -->
+
+				<div class="column three last">
+
+
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					1:00pm - 9:00pm<br><br>
+
+					<em>Friday - Saturday</em><br>
+					4:00pm - 11:00pm<br><br>
+
+					<em>Sunday - Monday</em><br>
+					Closed<br>
+
+					<br>
+
+					<?php include('includes/store-hours.php'); ?>
+
+				</div><!-- column -->
+
+			</div><!-- footer -->
+
+			<small>&copy;<?php echo date('Y'); ?> <a href="http://bradhussey.ca/course/codephp/"><?php echo $companyName; ?></a></small>
+
+		</div><!-- content -->
+
+	</div><!-- wrapper -->
+
+	<div class="copyright-info">
+		<?php include('assets/includes/copyright.php'); ?>
+	</div><!-- copyright-info -->
+</body>
+</html>
